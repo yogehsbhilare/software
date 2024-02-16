4f03f6f18920b0d4edfd87fc809bc27402f675f4
Yogeshbhilare <157691116+yogehsbhilare@users.noreply.github.com>
2024-02-16 09:53:30 +0530

    Update install_goss.sh

--- install_goss.sh
+++ install_goss.sh
@@ -1,7 +1,9 @@
-!/bin/bash
-set -xv
-curl -L https://github.com/goss-org/goss/releases/latest/download/goss-linux-amd64 -o /usr/local/bin/goss
-chmod +rx /usr/local/bin/goss
-curl -L https://github.com/goss-org/goss/releases/latest/download/dgoss  -o /usr/local/bin/dgoss
-chmod +rx /usr/local/bin/dgoss
-set +xv
+#!/bin/bash
+
+if ! [ -f /usr/local/bin/goss ]; then
+        set -xv
+        curl -L https://github.com/goss-org/goss/releases/latest/download/goss-linux-amd64 -o /usr/local/bin/goss
+        chmod +rx /usr/local/bin/goss
+        curl -L https://github.com/goss-org/goss/releases/latest/download/dgoss  -o /usr/local/bin/dgoss
+        chmod +rx /usr/local/bin/dgoss
+fi
